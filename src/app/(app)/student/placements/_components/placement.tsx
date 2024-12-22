bb3e9bc0eedf02b2eb719ea1b9a4d4ef4c96ebda
Sresta Malla <mallasresta@gmail.com>
2024-12-23 02:21:25 +0545

    chore:packag updates and commented out unused function

--- src/app/(app)/student/placements/_components/placement.tsx
+++ src/app/(app)/student/placements/_components/placement.tsx
@@ -16,13 +16,13 @@ import React from "react";
 import { useDropzone } from "react-dropzone";
 
 const Placement = () => {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const { getRootProps, getInputProps } = useDropzone();
 
-  const files = acceptedFiles.map((file) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+  // const files = acceptedFiles.map((file) => (
+  //   <li key={file.path}>
+  //     {file.path} - {file.size} bytes
+  //   </li>
+  // ));
 
   return (
     <>
