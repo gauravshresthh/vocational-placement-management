a9856705d8cbe632e7a8891c63fbcc15bf535eff
gauravshresthh <gauravshresthh@gmail.com>
2024-12-29 15:16:05 +0545

    feat: login and redux setup, also integrated backend

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+const axiosInstance = axios.create({
+    baseURL: `${BASE_URL}/api/v1`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+export default axiosInstance;
---
+++ src/store/store.tsx
@@ -0,0 +1,15 @@
+// /src/redux/store.ts
+'use client';
+
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+// Export types for use in components
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
